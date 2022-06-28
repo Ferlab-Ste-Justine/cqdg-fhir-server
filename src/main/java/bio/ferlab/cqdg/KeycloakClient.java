819db37f34febf31243424f3078f323257abc394
adrian <adipaul1981@yahoo.ca>
2022-06-28 11:35:28 -0400

    keycloack auth

---
+++ src/main/java/bio/ferlab/cqdg/KeycloakClient.java
@@ -0,0 +1,66 @@
+package bio.ferlab.cqdg;
+
+import bio.ferlab.cqdg.properties.BioProperties;
+import com.github.dnault.xmlpatch.internal.Log;
+import org.keycloak.authorization.client.AuthzClient;
+import org.keycloak.authorization.client.Configuration;
+import org.keycloak.authorization.client.representation.TokenIntrospectionResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class KeycloakClient {
+
+	private static final Logger log = LoggerFactory.getLogger(KeycloakClient.class);
+
+	public static final String AUTH_CLIENT_SECRET_KEY = "secret";
+
+	private final BioProperties bioProperties;
+
+	private AuthzClient authzClient;
+
+	public KeycloakClient(BioProperties bioProperties) {
+		this.bioProperties = bioProperties;
+	}
+
+	public TokenIntrospectionResponse introspectRpt(String rpt) {
+		return this.retryableIntrospectRpt(rpt, bioProperties.getAuthRetry());
+	}
+
+	private TokenIntrospectionResponse retryableIntrospectRpt(String rpt, int retry) {
+		try {
+			return this.getAuthzClient().protection().introspectRequestingPartyToken(rpt);
+		} catch (Exception e) {
+			log.error("Failed to introspect token: {}", e.getMessage());
+			if (retry > 0) {
+				log.info("Retry to introspect token: {}", retry);
+				return retryableIntrospectRpt(rpt, retry - 1);
+			} else {
+				throw e;
+			}
+		}
+	}
+
+	private AuthzClient initAuthzClient() {
+		try {
+			Log.info("Init auth. client");
+			final Configuration configuration = new Configuration();
+			configuration.setRealm(bioProperties.getAuthRealm());
+			configuration.setAuthServerUrl(bioProperties.getAuthServerUrl());
+			configuration.setResource(bioProperties.getAuthClientId());
+			configuration.getCredentials().put(AUTH_CLIENT_SECRET_KEY, bioProperties.getAuthClientSecret());
+			this.authzClient = AuthzClient.create(configuration);
+			return this.authzClient;
+		} catch (Exception e) {
+			log.error("Failed to init auth. client", e);
+			throw e;
+		}
+	}
+
+	private AuthzClient getAuthzClient() {
+		return Optional.ofNullable(this.authzClient).orElseGet(this::initAuthzClient);
+	}
+}
---
+++ src/main/java/bio/ferlab/cqdg/properties/BioProperties.java
@@ -0,0 +1,90 @@
+package bio.ferlab.cqdg.properties;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Optional;
+
+@Configuration
+@ConfigurationProperties(prefix = "bio")
+@Data
+public class BioProperties {
+    private final boolean isBioEsEnabled;
+    private final String esHost;
+    private final int esPort;
+    private final String esScheme;
+    private final String esPatientsIndex;
+    private final String esPrescriptionsIndex;
+    private final boolean isAuthEnabled;
+    private final String authServerUrl;
+    private final String authRealm;
+    private final Integer authRetry;
+    private final Long authLeeway;
+    private final boolean isDisableSslValidation;
+    private final boolean isAuditsEnabled;
+    private final boolean isTaggingEnabled;
+    private final boolean isServiceRequestRoutingEnabled;
+    private final boolean isTaggingQueryParam;
+    private final boolean isAuthorizationEnabled;
+    private final String authClientId;
+    private final String authSystemId;
+    private final String authClientSecret;
+    private final boolean isNanuqEnabled;
+    private final String nanuqEsAnalysesIndex;
+    private final String nanuqEsSequencingsIndex;
+
+    public BioProperties(
+            @Value("${bio.elasticsearch.enabled}") boolean isBioEsEnabled,
+            @Value("${bio.elasticsearch.host}") String esHost,
+            @Value("${bio.elasticsearch.port}") int esPort,
+            @Value("${bio.elasticsearch.scheme}") String esScheme,
+            @Value("${bio.elasticsearch.patients-index}") String esPatientsIndex,
+            @Value("${bio.elasticsearch.prescriptions-index}") String esPrescriptionsIndex,
+            @Value("${bio.auth.enabled}") boolean isAuthEnabled,
+            @Value("${bio.auth.server-url}") String authServerUrl,
+            @Value("${bio.auth.realm}") String authRealm,
+            @Value("${bio.auth.retry}") Integer authRetry,
+            @Value("${bio.auth.leeway}") Long authLeeway,
+            @Value("${bio.auth.disable-ssl-validation}") boolean isDisableSslValidation,
+            @Value("${bio.audits.enabled}") boolean isAuditsEnabled,
+            @Value("${bio.tagging.enabled}") boolean isTaggingEnabled,
+            @Value("${bio.tagging.queryParam}") boolean isTaggingQueryParam,
+            @Value("${bio.auth.authorization.enabled}") boolean isAuthorizationEnabled,
+            @Value("${bio.service-request-routing.enabled}") boolean isServiceRequestRoutingEnabled,
+            @Value("${bio.auth.authorization.client-id}") String authClientId,
+            @Value("${bio.auth.authorization.client-secret}") String authClientSecret,
+            @Value("${bio.auth.authorization.system-id}") String authSystemId,
+            @Value("${bio.nanuq.enabled}") boolean isNanuqEnabled,
+            @Value("${bio.nanuq.analyses-index}") String nanuqEsAnalysesIndex,
+            @Value("${bio.nanuq.sequencings-index}") String nanuqEsSequencingsIndex
+
+    ) {
+        this.isBioEsEnabled = isBioEsEnabled;
+        this.esHost = esHost;
+        this.esPort = esPort;
+        this.esScheme = esScheme;
+        this.esPatientsIndex = esPatientsIndex;
+        this.esPrescriptionsIndex = esPrescriptionsIndex;
+        this.isAuthEnabled = isAuthEnabled;
+        this.authServerUrl = authServerUrl;
+        this.authRealm = authRealm;
+        this.authRetry = authRetry;
+        this.authLeeway = Optional.ofNullable(authLeeway).orElse(0L);
+        this.isDisableSslValidation = isDisableSslValidation;
+        this.isAuditsEnabled = isAuditsEnabled;
+        this.isTaggingEnabled = isTaggingEnabled;
+        this.isTaggingQueryParam = isTaggingQueryParam;
+        this.isAuthorizationEnabled = isAuthorizationEnabled;
+        this.authClientId = authClientId;
+        this.authClientSecret = authClientSecret;
+        this.authSystemId = authSystemId;
+        this.isNanuqEnabled = isNanuqEnabled;
+        this.nanuqEsAnalysesIndex = nanuqEsAnalysesIndex;
+        this.nanuqEsSequencingsIndex = nanuqEsSequencingsIndex;
+        this.isServiceRequestRoutingEnabled = isServiceRequestRoutingEnabled;
+    }
+
+
+}
