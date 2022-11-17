316120f9678dd849f1a5b1af5e50805cc770b585
adrian <adipaul1981@yahoo.ca>
2022-11-17 15:42:36 -0500

    fix: CQDG-0 display code and display

--- src/main/java/bio/ferlab/cqdg/auth/UserPermissionsBuilder.java
+++ src/main/java/bio/ferlab/cqdg/auth/UserPermissionsBuilder.java
@@ -12,14 +12,14 @@ import java.util.Set;
 public class UserPermissionsBuilder {
     private static final Logger log = LoggerFactory.getLogger(UserPermissionsBuilder.class);
     public static final String RESOURCES_MODULE = "org.hl7.fhir.r4.model";
-    public static final String CUSTOM_RESOURCES_MODULE = "bio.ferlab.clin.auth.data.custom";
+    public static final String CUSTOM_RESOURCES_MODULE = "bio.ferlab.cqdg.auth.data.custom";
     public static final String READ_SCOPE = "read";
     public static final String UPDATE_SCOPE = "update";
     public static final String CREATE_SCOPE = "create";
     public static final String DELETE_SCOPE = "delete";
 
     private final List<Permission<? extends BaseResource>> permissions = new ArrayList<>();
-    
+
     private Permission<? extends BaseResource> permissionFromResourceName(String resource, Set<String> scopes) {
         final var completeName = String.format("%s.%s", RESOURCES_MODULE, resource);
         try {
@@ -51,7 +51,7 @@ public class UserPermissionsBuilder {
         final var completeName = String.format("%s.%s", rootPackage, className);
         return (Class<? extends BaseResource>) Class.forName(completeName);
     }
-    
+
     private String sanitizeResourceName(String resource) {
         return StringUtils.capitalize(StringUtils.trimToEmpty(resource).replaceAll("[^A-Za-z0-9]",""));
     }
